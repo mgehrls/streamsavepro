27afd2507936d54e9274325d8f624512af439780
Matt Gehrls <mattgehrls@gmail.com>
2022-11-17 13:43:43 -0500

    prisma update + prisma orderby play

--- src/server/trpc/router/listItem.ts
+++ src/server/trpc/router/listItem.ts
@@ -10,7 +10,7 @@ export const listItemRouter = router({
          },include:{
            media:true
          }, orderBy:{
-           lastSeen: "asc"
+           lastSeen:"asc"
          }})
     }),
   removeListItem: protectedProcedure
